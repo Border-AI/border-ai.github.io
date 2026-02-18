066e4d0c82d9f50d6e1d35a17b85b33c33729273
Zahra-Razavi <104886755+Zahra-Razavi@users.noreply.github.com>
2026-02-18 15:51:47 +0330

    merge app and site

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { resolve } from "path";
 
 export default defineConfig({
-  base: "/",
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  root: resolve(__dirname, "app-shell"),
+  base: "/app/",
+  publicDir: resolve(__dirname, "public"),
+  build: {
+    outDir: resolve(__dirname, "app"),
+    emptyOutDir: true,
+  },
+  plugins: [react(), tailwindcss()],
 });
